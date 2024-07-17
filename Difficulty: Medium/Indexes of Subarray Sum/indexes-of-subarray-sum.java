ff23cfebaadbe68515fdfabc0b08639f8cd164c6
Kumar Sahil <123011559+SahilCharan@users.noreply.github.com>
2024-07-18 00:03:07 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Indexes of Subarray Sum/indexes-of-subarray-sum.java
@@ -0,0 +1,77 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+class IntArray {
+    public static int[] input(BufferedReader br, int n) throws IOException {
+        String[] s = br.readLine().trim().split(" ");
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++) a[i] = Integer.parseInt(s[i]);
+
+        return a;
+    }
+
+    public static void print(int[] a) {
+        for (int e : a) System.out.print(e + " ");
+        System.out.println();
+    }
+
+    public static void print(ArrayList<Integer> a) {
+        for (int e : a) System.out.print(e + " ");
+        System.out.println();
+    }
+}
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int t;
+        t = Integer.parseInt(br.readLine());
+        while (t-- > 0) {
+            String str[] = br.readLine().trim().split(" ");
+
+            int n = Integer.parseInt(str[0]);
+            int s = Integer.parseInt(str[1]);
+
+            int[] arr = IntArray.input(br, n);
+
+            Solution obj = new Solution();
+            ArrayList<Integer> res = obj.subarraySum(arr, n, s);
+
+            IntArray.print(res);
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+
+class Solution {
+    public static ArrayList<Integer> subarraySum(int[] arr, int n, int s) {
+        // code here
+        int i =0, j =0, sum =0;
+        ArrayList<Integer> list = new ArrayList<>();
+        while(j<n)
+        {
+            sum+=arr[j];
+            
+            while(sum>s && i<j)
+            {
+                sum-=arr[i];
+                i++;
+            }
+            
+            if(sum == s)
+            {
+                list.add(i+1);
+                list.add(j+1);
+                return list;
+            }
+            j++;
+        }
+        list.add(-1);
+        return list;
+    }
+}
